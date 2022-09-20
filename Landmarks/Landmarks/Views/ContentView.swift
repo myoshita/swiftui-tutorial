35fb44096e883d56790555c08cfc297112893027
Masataka Yoshita <yoshita_masataka@andfactory.co.jp>
2022-09-20 16:31:31 +0900

    Complete Building Lists And Navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Masataka Yoshita on 2022/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
