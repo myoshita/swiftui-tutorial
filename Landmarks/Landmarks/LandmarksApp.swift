152c3c0eb07f2ddf71c5adfa09d652e85f40d538
Masataka Yoshita <yoshita_masataka@andfactory.co.jp>
2022-09-20 15:07:27 +0900

    Add Landmarks
    Creating and Combining Views

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Masataka Yoshita on 2022/09/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
